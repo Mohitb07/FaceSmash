44c09c17a37d7e922be9274b55214792fa758529
mohitb07 <bmohit980@gmail.com>
2022-04-18 21:35:32 +0530

    native base theme config

---
+++ src/theme/index.js
@@ -0,0 +1,27 @@
+import {extendTheme} from 'native-base';
+
+export const theme = extendTheme({
+  colors: {
+    // Add new color
+    primary: {
+      50: '#E3F2F9',
+      100: '#C5E4F3',
+      200: '#A2D4EC',
+      300: '#7AC1E4',
+      400: '#47A9DA',
+      500: '#0088CC',
+      600: '#007AB8',
+      700: '#006BA1',
+      800: '#005885',
+      900: '#003F5E',
+    },
+    // Redefinig only one shade, rest of the color will remain same.
+    amber: {
+      400: '#d97706',
+    },
+  },
+  config: {
+    // Changing initialColorMode to 'dark'
+    initialColorMode: 'dark',
+  },
+});
