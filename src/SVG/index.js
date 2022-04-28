885ee6132eb7c3f2d1fd85b932485672b20f0639
mohitb07 <bmohit980@gmail.com>
2022-04-28 21:29:08 +0530

    Home icon added and icon size reduced

--- src/SVG/index.js
+++ src/SVG/index.js
@@ -300,8 +300,8 @@ export const GearIcon = props => (
 
 export const LogoutIcon = props => (
   <Svg
-    width="24px"
-    height="24px"
+    width="15px"
+    height="20px"
     viewBox="0 0 24 24"
     id="magicoon-Filled"
     xmlns="http://www.w3.org/2000/svg"
@@ -321,8 +321,8 @@ export const LogoutIcon = props => (
 
 export const PrivacyIcon = props => (
   <Svg
-    width="24px"
-    height="24px"
+    width="15px"
+    height="20px"
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     {...props}>
@@ -337,8 +337,8 @@ export const PrivacyIcon = props => (
 
 export const DocumentIcon = props => (
   <Svg
-    width="24px"
-    height="24px"
+    width="15px"
+    height="20px"
     viewBox="-1.27 0 30 30"
     id="_14_-_Document"
     data-name="14 - Document"
@@ -383,6 +383,21 @@ export const DocumentIcon = props => (
       d="M7.091,21.455H19.818a1,1,0,0,0,0-2H7.091a1,1,0,0,0,0,2Z"
       transform="translate(-2.273 -1)"
       fillRule="evenodd"
+    />
+  </Svg>
+);
+
+export const HomeIcon = props => (
+  <Svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}>
+    <Path
+      d="M13.3758 3.47015C12.5655 2.84328 11.4345 2.84328 10.6242 3.47015L3.87424 8.69205C3.32286 9.11861 3 9.77686 3 10.4745V18.7474C3 19.9915 4.00736 21 5.25 21H8.25C9.49264 21 10.5 19.9915 10.5 18.7474V16.4948C10.5 15.6655 11.1716 14.9931 12 14.9931C12.8284 14.9931 13.5 15.6655 13.5 16.4948V18.7474C13.5 19.9915 14.5074 21 15.75 21H18.75C19.9926 21 21 19.9915 21 18.7474V10.4745C21 9.77686 20.6771 9.11861 20.1258 8.69205L13.3758 3.47015Z"
+      fill="#3A52EE"
     />
   </Svg>
 );
