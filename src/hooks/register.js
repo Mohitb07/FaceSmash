698a4d0cce245d8f5b4b59add4042ab7bb8eccbe
mohitb07 <bmohit980@gmail.com>
2022-05-24 21:48:05 +0530

    Custom user registeration hook

---
+++ src/hooks/register.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+export function useRegister() {
+  const onRegisterAttempt = ({email = '', password = '', username = ''}) => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        console.log('inside user', user.user.metadata);
+        firestore()
+          .collection('Users')
+          .doc(user.user.uid)
+          .set({
+            email: user.user.email,
+            username: username,
+            password: password,
+            uid: user.user.uid,
+            followers: [],
+            followings: [],
+            createdAt: user.user.metadata.creationTime,
+            lastSignIn: user.user.metadata.lastSignInTime,
+          })
+          .then(() => {
+            console.log('User added!');
+          });
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+
+  return onRegisterAttempt;
+}
