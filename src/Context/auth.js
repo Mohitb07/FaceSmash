4460c5314b1046ecee9ff54c1b9e25c05e49dfdd
mohitb07 <bmohit980@gmail.com>
2022-05-24 21:47:25 +0530

    fix: user null problem when re rendered

--- src/Context/auth.js
+++ src/Context/auth.js
@@ -4,34 +4,30 @@ import auth from '@react-native-firebase/auth';
 export const AuthContext = createContext();
 
 function AuthProvider({children}) {
+  const [authUser, setAuthUser] = useState();
   const [initializing, setInitializing] = useState(true);
-  const [authUser, setAuthUser] = useState(null);
 
-  // function onAuthStateChanged(authUser) {
-  //   setAuthUser(authUser);
-  //   if (initializing) setInitializing(false);
-  // }
-
-  // useEffect(() => {
-  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-  //   return subscriber;
-  // }, []);
-
-  const setAuthenticatedUser = value => {
-    setAuthUser(value);
-  };
-
-  if (!authUser) {
-    console.log('user not authenticated');
+  function onAuthStateChanged(authUser) {
+    setAuthUser(authUser);
+    if (initializing) setInitializing(false);
   }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  // const setAuthenticatedUser = value => {
+  //   setAuthUser(value);
+  // };
+
+  console.log('authuser', authUser);
 
   if (authUser) {
-    console.log('user is authenticated', authUser);
+    console.log('user is authenticated');
   }
   return (
-    <AuthContext.Provider value={{authUser, setAuthenticatedUser}}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{authUser}}>{children}</AuthContext.Provider>
   );
 }
 
