dc7779fcdb01193c7f56f4ec3af820ea14879c6c
mohitb07 <bmohit980@gmail.com>
2022-06-23 22:06:44 +0530

    svgs added

--- src/SVG/index.js
+++ src/SVG/index.js
@@ -580,3 +580,24 @@ export const EditIcon = props => (
     />
   </Svg>
 );
+
+export const AddIcon = props => (
+  <Svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    id="magicoon-Regular"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}>
+    <Defs></Defs>
+    <G id="plus-square-Regular">
+      <Path
+        id="plus-square-Regular-2"
+        data-name="plus-square-Regular"
+        className="cls-1"
+        d="M15,2.25H9A6.758,6.758,0,0,0,2.25,9v6A6.758,6.758,0,0,0,9,21.75h6A6.758,6.758,0,0,0,21.75,15V9A6.758,6.758,0,0,0,15,2.25ZM20.25,15A5.256,5.256,0,0,1,15,20.25H9A5.256,5.256,0,0,1,3.75,15V9A5.256,5.256,0,0,1,9,3.75h6A5.256,5.256,0,0,1,20.25,9Zm-3.5-3a.75.75,0,0,1-.75.75H12.75V16a.75.75,0,0,1-1.5,0V12.75H8a.75.75,0,0,1,0-1.5h3.25V8a.75.75,0,0,1,1.5,0v3.25H16A.75.75,0,0,1,16.75,12Z"
+        fill="#fff"
+      />
+    </G>
+  </Svg>
+);
