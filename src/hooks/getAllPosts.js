2382a8245a17c448ac7b991bb22359ebfe077f9c
mohitb07 <bmohit980@gmail.com>
2022-06-29 22:19:17 +0530

    fix: null user when logged out

--- src/hooks/getAllPosts.js
+++ src/hooks/getAllPosts.js
@@ -1,51 +1,59 @@
 import firestore from '@react-native-firebase/firestore';
 import {useContext, useEffect, useState} from 'react';
+import {AuthContext} from '../Context/auth';
 import {UserDataContext} from '../Context/userData';
 
 const useGetAllPosts = () => {
+  console.log('inside useGetAllPosts');
   const {contextUser} = useContext(UserDataContext);
+  const {authUser} = useContext(AuthContext);
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const getLatestPosts = () => {
-    firestore()
-      .collection('Posts')
-      .orderBy('createdAt', 'desc')
-      .onSnapshot(querySnapshot => {
-        const allPosts = [];
+    if (authUser) {
+      firestore()
+        .collection('Posts')
+        .orderBy('createdAt', 'desc')
+        .onSnapshot(querySnapshot => {
+          const allPosts = [];
 
-        querySnapshot.forEach(documentSnapshot => {
-          allPosts.push({
-            ...documentSnapshot.data(),
-            key: documentSnapshot.id,
+          querySnapshot.forEach(documentSnapshot => {
+            allPosts.push({
+              ...documentSnapshot.data(),
+              key: documentSnapshot.id,
+            });
           });
-        });
 
-        setPosts(allPosts);
-        setLoading(false);
-      });
+          setPosts(allPosts);
+          setLoading(false);
+        });
+    }
   };
 
+  let sub;
   useEffect(() => {
-    const subscriber = firestore()
-      .collection('Posts')
-      .orderBy('createdAt', 'desc')
-      .onSnapshot(querySnapshot => {
-        const allPosts = [];
+    if (authUser) {
+      sub = firestore()
+        .collection('Posts')
+        .orderBy('createdAt', 'desc')
+        .onSnapshot(querySnapshot => {
+          const allPosts = [];
 
-        querySnapshot.forEach(documentSnapshot => {
-          allPosts.push({
-            ...documentSnapshot.data(),
-            key: documentSnapshot.id,
+          querySnapshot.forEach(documentSnapshot => {
+            allPosts.push({
+              ...documentSnapshot.data(),
+              key: documentSnapshot.id,
+            });
           });
+
+          setPosts(allPosts);
+          setLoading(false);
         });
+    }
 
-        setPosts(allPosts);
-        setLoading(false);
-      });
-
-    return () => subscriber();
-  }, [contextUser]);
+    return () => sub();
+  }, []);
 
   return [posts, loading, contextUser, getLatestPosts, setPosts, setLoading];
 };
