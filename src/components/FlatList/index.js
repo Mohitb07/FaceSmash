fa3fd2cee0df6ccede2e8e2ee2186b2d0497704a
mohitb07 <bmohit980@gmail.com>
2022-07-05 23:18:56 +0530

    Code refactored

--- src/components/FlatList/index.js
+++ src/components/FlatList/index.js
@@ -1,15 +1,100 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {FlatList, RefreshControl} from 'react-native';
-import useGetAllPosts from '../../hooks/getAllPosts';
 import Feed from '../Feed';
 import PostHeader from '../PostHeader';
+import {useRecoilState} from 'recoil';
+import {postState} from '../../atoms/postAtom';
+import firestore from '@react-native-firebase/firestore';
+import usePosts from '../../hooks/usePosts';
+import {authState} from '../../atoms/authAtom';
 
 function CustomFlatList({navigation}) {
-  const [memoPosts, onRefresh, refreshing, loading] = useGetAllPosts();
+  const [onPostLike] = usePosts();
+  const [postStateValue, setPostStateValue] = useRecoilState(postState);
+  const [authUser, setAuthUser] = useRecoilState(authState);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const getPosts = useCallback(async () => {
+    try {
+      const allPosts = await firestore()
+        .collection('Posts')
+        .orderBy('createdAt', 'desc')
+        .get();
+
+      const latestPost = [];
+      allPosts.docs.map(item => {
+        latestPost.push({
+          ...item.data(),
+          key: item.id,
+        });
+      });
+
+      setPostStateValue(prev => ({
+        ...prev,
+        posts: latestPost,
+      }));
+      setLoading(false);
+    } catch (error) {
+      console.log('getPosts error', error);
+      setLoading(false);
+    }
+    // .onSnapshot(querySnapshot => {
+    //   const allPosts = [];
+
+    //   querySnapshot.forEach(documentSnapshot => {
+    //     allPosts.push({
+    //       ...documentSnapshot.data(),
+    //       key: documentSnapshot.id,
+    //     });
+    //   });
+
+    //   setPostStateValue(allPosts);
+    //   setLoading(false);
+    // });
+  }, []);
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await getPosts();
+    setRefreshing(false);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  useEffect(() => {
+    async function getUserLikedPosts() {
+      // get user's liked posts
+      try {
+        const userLikedPosts = await firestore()
+          .collection('Users')
+          .doc(authUser.uid)
+          .collection('postlikes')
+          .get();
+
+        const postsLiked = [];
+        userLikedPosts.docs.map(doc => postsLiked.push(doc.data()));
+
+        setPostStateValue(prev => ({
+          ...prev,
+          postLikes: postsLiked,
+        }));
+      } catch (err) {
+        console.log('getLikedposterror', err.message);
+      }
+    }
+
+    getUserLikedPosts();
+  }, []);
+
+  console.log('state', postStateValue.posts);
+
   return (
     <FlatList
       showsVerticalScrollIndicator={false}
-      data={memoPosts}
+      data={postStateValue.posts}
       keyExtractor={item => item.key}
       refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
@@ -27,8 +112,14 @@ function CustomFlatList({navigation}) {
           image={item.image}
           description={item.description}
           navigation={navigation}
-          likes={item.likes}
+          likes={
+            postStateValue.posts.find(post => post.key === item.key)?.likes
+          }
           userId={item.user}
+          onLike={onPostLike}
+          post={item}
+          // onDelete={onPostDelete}
+          // onUpdate={onPostUpdate}
         />
       )}
     />
