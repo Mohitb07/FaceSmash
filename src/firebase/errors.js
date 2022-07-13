c34ee46b759003736da4f519048949ca9c1959e9
mohitb07 <bmohit980@gmail.com>
2022-07-13 22:05:54 +0530

    firebase errors

--- src/firebase/errors.js
+++ src/firebase/errors.js
@@ -1,5 +1,5 @@
 export const FIREBASE_ERRORS = {
-  'Firebase: Error (auth/email-already-in-use).':
+  '[auth/email-already-in-use] The email address is already in use by another account.':
     'A user with that email already exists',
 
   '[auth/user-not-found] There is no user record corresponding to this identifier. The user may have been deleted.':
@@ -7,4 +7,6 @@ export const FIREBASE_ERRORS = {
   '[auth/wrong-password] The password is invalid or the user does not have a password.':
     'Invalid email or password',
   '[auth/invalid-email] The email address is badly formatted.': 'Invalid email',
+  '[auth/weak-password] The given password is invalid. [ Password should be at least 6 characters ]':
+    'Password should be at least 6 characters',
 };
