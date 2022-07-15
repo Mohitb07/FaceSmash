30a18e009668949acf08e98137c22dc39da8e17c
mohitb07 <bmohit980@gmail.com>
2022-07-15 15:43:17 +0530

    for users liked posts

---
+++ src/hooks/useLikedPosts.js
@@ -0,0 +1,41 @@
+import React, {useCallback, useContext, useEffect, useState} from 'react';
+
+import firestore from '@react-native-firebase/firestore';
+
+import {AuthUserContext} from '../Context/auth';
+
+const useLikedPosts = () => {
+  const {authUser} = useContext(AuthUserContext);
+  const [userLikedPosts, setUserLikedPosts] = useState([]);
+  const [error, setError] = useState('');
+
+  const getUserLikedPosts = useCallback(async () => {
+    try {
+      const userLikedPosts = await firestore()
+        .collection('Users')
+        .doc(authUser?.uid)
+        .collection('postlikes')
+        .get();
+
+      const postsLiked = [];
+      userLikedPosts.docs.map(doc => postsLiked.push(doc.data()));
+
+      setUserLikedPosts(postsLiked);
+    } catch (err) {
+      console.log('getLikedposterror', err.message);
+      setError(err.message);
+    }
+  }, []);
+
+  const refetch = useCallback(async () => {
+    getUserLikedPosts();
+  }, []);
+
+  useEffect(() => {
+    getUserLikedPosts();
+  }, [getUserLikedPosts]);
+
+  return {userLikedPosts, refetch, error};
+};
+
+export default useLikedPosts;
