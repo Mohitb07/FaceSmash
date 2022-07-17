ca81e0168eb2cd41686983760698ed1945f35157
mohitb07 <bmohit980@gmail.com>
2022-07-17 22:01:40 +0530

    fix: authUser was null while attempting to update the user

--- src/Context/userData.js
+++ src/Context/userData.js
@@ -1,17 +1,12 @@
 import React, {useEffect, useState, useContext, useCallback} from 'react';
 import firestore from '@react-native-firebase/firestore';
-// import {AuthContext} from './auth';
-import storage from '@react-native-firebase/storage';
-import {useRecoilState} from 'recoil';
-import {authState, initializingState} from '../atoms/authAtom';
-import auth from '@react-native-firebase/auth';
 import {AuthUserContext} from './auth';
 
 export const UserDataContext = React.createContext();
 
 const UserDataProvider = props => {
   console.log('user data context render');
-  const [contextUser, setContextUser] = useState({});
+  const [contextUser, setContextUser] = useState(null);
   const {authUser} = useContext(AuthUserContext);
 
   // function onAuthStateChanged(user) {
@@ -38,50 +33,129 @@ const UserDataProvider = props => {
     getData();
   }, [authUser]);
 
-  const updateUserData = useCallback((url, navigation, setLoading) => {
-    firestore()
-      .collection('Users')
-      .doc(authUser?.uid)
-      .update({
-        profilePic: url,
-      })
-      .then(async () => {
+  console.log('authUser outside', authUser);
+
+  const updateUserData = useCallback(
+    async (url, navigation, setLoading, userId) => {
+      console.log('authuser insdier', userId);
+      if (userId) {
+        const userRef = firestore().collection('Users').doc(userId);
+
+        await userRef.update({
+          profilePic: url,
+        });
+        const batch = firestore().batch();
+
         const updatedUser = await firestore()
           .collection('Users')
-          .doc(authUser?.uid)
+          .doc(userId)
           .get();
-        setContextUser(updatedUser.data());
-        const userRef = await firestore()
-          .collection('Users')
-          .doc(authUser?.uid)
-          .get();
-        console.log('user ref ⚡⚡⚡⚡⚡', userRef);
+
+        console.log('updated User', updatedUser);
+
         const allPosts = await firestore()
           .collection('Posts')
-          .where('user', '==', authUser?.uid)
+          .where('user', '==', userId)
           .orderBy('createdAt', 'desc')
           .get();
 
-        const batch = firestore().batch();
-        allPosts.forEach(doc => {
+        allPosts.docs.forEach(doc => {
           const docRef = firestore().collection('Posts').doc(doc.id);
           batch.update(docRef, {
-            userProfile: userRef.data().profilePic,
+            userProfile: updatedUser.data().profilePic,
           });
         });
-        batch.commit().then(() => {
-          console.log('updated al docs');
-          setLoading(false);
-          navigation.replace('Profile', {
-            providedUserId: authUser?.uid,
+
+        batch
+          .commit()
+          .then(() => {
+            setLoading(false);
+            setContextUser(updatedUser.data());
+            navigation.navigate('Profile', {
+              providedUserId: userId,
+            });
+          })
+          .catch(err => {
+            throw Error(err.message);
           });
-        });
-      })
-      .catch(error => {
-        console.log('ERROR UPDATING USER', error);
-        return null;
-      });
-  }, []);
+      }
+
+      // const updatedUserRef = await firestore()
+      //   .collection('Users')
+      //   .doc(authUser?.uid)
+      //   .get();
+
+      // const allPosts = await firestore()
+      //   .collection('Posts')
+      //   .where('user', '==', authUser?.uid)
+      //   .orderBy('createdAt', 'desc')
+      //   .get();
+
+      // const batch = firestore().batch();
+      // allPosts.docs.forEach(doc => {
+      //   const docRef = firestore().collection('Posts').doc(doc.id);
+      //   batch.update(docRef, {
+      //     userProfile: updatedUserRef.data().profilePic,
+      //   });
+      // });
+
+      // batch
+      //   .commit()
+      //   .then(() => {
+      //     setLoading(false);
+      //     navigation.navigate('Profile', {
+      //       providedUserId: authUser?.uid,
+      //     });
+      //   })
+      //   .catch(err => {
+      //     throw Error(err.message);
+      //   });
+
+      // firestore()
+      //   .collection('Users')
+      //   .doc(authUser?.uid)
+      //   .update({
+      //     profilePic: url,
+      //   })
+      //   .then(async () => {
+      //     const updatedUser = await firestore()
+      //       .collection('Users')
+      //       .doc(authUser?.uid)
+      //       .get();
+      //     setContextUser(updatedUser.data());
+      //     const userRef = await firestore()
+      //       .collection('Users')
+      //       .doc(authUser?.uid)
+      //       .get();
+      //     console.log('user ref ⚡⚡⚡⚡⚡', userRef);
+      //     const allPosts = await firestore()
+      //       .collection('Posts')
+      //       .where('user', '==', authUser?.uid)
+      //       .orderBy('createdAt', 'desc')
+      //       .get();
+
+      //     const batch = firestore().batch();
+      //     allPosts.forEach(doc => {
+      //       const docRef = firestore().collection('Posts').doc(doc.id);
+      //       batch.update(docRef, {
+      //         userProfile: userRef.data().profilePic,
+      //       });
+      //     });
+      //     batch.commit().then(() => {
+      //       console.log('updated al docs');
+      //       setLoading(false);
+      //       navigation.replace('Profile', {
+      //         providedUserId: authUser?.uid,
+      //       });
+      //     });
+      //   })
+      //   .catch(error => {
+      //     console.log('ERROR UPDATING USER', error);
+      //     return null;
+      //   });
+    },
+    [],
+  );
 
   const value = React.useMemo(
     () => ({
