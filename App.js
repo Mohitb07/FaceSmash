eec1d9b51bb3da378123acfdab218a1c57107d7b
mohitb07 <bmohit980@gmail.com>
2022-07-20 23:10:29 +0530

    splash screen configuration

--- App.js
+++ App.js
@@ -6,20 +6,27 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
+
+import {NativeBaseProvider} from 'native-base';
+import {RecoilRoot} from 'recoil';
+import Toast from 'react-native-toast-message';
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
+
 import Navigation, {navigationRef} from './src/Navigation/Root';
 import UserDataContext from './src/Context/userData';
 import BottomSheetProvider from './src/Context/BottomSheet';
-import {NativeBaseProvider} from 'native-base';
 import {theme} from './src/theme';
-import {RecoilRoot} from 'recoil';
-import 'react-native-gesture-handler';
 import AuthUserContext from './src/Context/auth';
 import BottomSheet from './src/components/BottomSheet';
-import Toast from 'react-native-toast-message';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <RecoilRoot>
