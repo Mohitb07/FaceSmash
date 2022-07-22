3cbcc9c42a53084024f4bdc5f81e59ed67e048b3
mohitb07 <bmohit980@gmail.com>
2022-07-22 23:41:27 +0530

    Infinite Scroll Implemented

--- src/components/FlatList/HomeFeed/index.js
+++ src/components/FlatList/HomeFeed/index.js
@@ -1,7 +1,7 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import {Text, View} from 'native-base';
 
-import {FlatList, RefreshControl} from 'react-native';
+import {ActivityIndicator, FlatList, RefreshControl} from 'react-native';
 import {useRecoilState} from 'recoil';
 import firestore from '@react-native-firebase/firestore';
 
@@ -12,13 +12,17 @@ import FeedSkeleton from '../../FeedSkeleton';
 import {COLORS} from '../../../constants';
 import useLikedPosts from '../../../hooks/useLikedPosts';
 
+const LIMIT = 5;
+
 function HomeFeed({navigation}) {
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
   const {userLikedPosts, refetch} = useLikedPosts();
+  const [lastVisible, setLastVisible] = useState(null);
 
   const getPosts = useCallback(async () => {
+    console.log('get posts called');
     setPostStateValue(prev => ({
       ...prev,
       posts: [],
@@ -27,6 +31,7 @@ function HomeFeed({navigation}) {
       const allPosts = await firestore()
         .collection('Posts')
         .orderBy('createdAt', 'desc')
+        .limit(LIMIT)
         .get();
 
       const latestPost = [];
@@ -37,16 +42,60 @@ function HomeFeed({navigation}) {
         });
       });
 
+      let lastVisibleDoc = allPosts.docs[allPosts.docs.length - 1];
+
+      console.log('last visible doc id in getPosts', lastVisibleDoc);
+
       setPostStateValue(prev => ({
         ...prev,
         posts: latestPost,
       }));
+      setLastVisible(lastVisibleDoc);
       setLoading(false);
     } catch (error) {
       console.log('getPosts error', error);
       setLoading(false);
     }
   }, []);
+
+  const retrieveMore = async () => {
+    console.log('invoked');
+    console.log('before invocation lastVisible value', lastVisible);
+    if (!lastVisible) {
+      setLoading(false);
+      return;
+    }
+    try {
+      setLoading(true);
+      const allPosts = await firestore()
+        .collection('Posts')
+        .orderBy('createdAt', 'desc')
+        .startAfter(lastVisible)
+        .limit(LIMIT)
+        .get();
+
+      const latestPost = [];
+      allPosts.docs.map(item => {
+        latestPost.push({
+          ...item.data(),
+          key: item.id,
+        });
+      });
+
+      let lastVisibleDoc = allPosts.docs[allPosts.docs.length - 1];
+
+      console.log('final result', [...postStateValue.posts, ...latestPost]);
+
+      setPostStateValue(prev => ({
+        ...prev,
+        posts: [...postStateValue.posts, ...latestPost],
+      }));
+      setLastVisible(lastVisibleDoc);
+      setLoading(false);
+    } catch (error) {
+      throw new Error('Retrieve More Error', error.message);
+    }
+  };
 
   useEffect(() => {
     getPosts();
@@ -106,12 +155,18 @@ function HomeFeed({navigation}) {
       ListFooterComponent={
         postStateValue.posts.length > 0 && (
           <View paddingY="5">
-            <Text textAlign="center" color="gray.500">
-              No More post
-            </Text>
+            {loading ? (
+              <FeedSkeleton />
+            ) : (
+              <Text textAlign="center" color="gray.500">
+                No More post
+              </Text>
+            )}
           </View>
         )
       }
+      onEndReached={retrieveMore}
+      onEndReachedThreshold={0}
       refreshControl={
         <RefreshControl
           progressBackgroundColor={COLORS.neon}
@@ -123,6 +178,7 @@ function HomeFeed({navigation}) {
         <HomeHeader navigation={navigation} loading={loading} />
       }
       renderItem={renderItem}
+      refreshing={loading}
     />
   );
 }
