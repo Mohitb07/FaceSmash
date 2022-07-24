affb227405e0fbb25d6b7e4a7e6b6344c384e8c5
mohitb07 <bmohit980@gmail.com>
2022-07-24 22:09:34 +0530

    Redesigning Home Screen

--- src/components/Header/Home/index.js
+++ src/components/Header/Home/index.js
@@ -1,15 +1,67 @@
-import {Avatar, Heading} from 'native-base';
+import {
+  Avatar,
+  Heading,
+  HStack,
+  Text as NText,
+  View as NView,
+  ScrollView,
+  TextField,
+  Input,
+} from 'native-base';
 import React, {useContext} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {COLORS} from '../../../constants';
 import {UserDataContext} from '../../../Context/userData';
-import {AddIcon} from '../../../SVG';
+import useSelectImage from '../../../hooks/useSelectImage';
+import {AddIcon, GallaryIcon, PhotoIcon, SearchIcon} from '../../../SVG';
+import Story from '../../Story';
 
 function PostHeader({navigation}) {
   const {contextUser} = useContext(UserDataContext);
+  const {handleChooseGallary} = useSelectImage();
+
+  const handleGetImageThenNavigate = () => {
+    // true for navigation
+    handleChooseGallary(true, navigation);
+  };
+
+  const dummyStoryData = [
+    {
+      uri: 'https://media.istockphoto.com/photos/portrait-of-a-young-african-man-at-studio-high-fashion-male-model-in-picture-id1325359218?b=1&k=20&m=1325359218&s=170667a&w=0&h=MflA10Erq46yR-LFSREN6svtgXP7OeKuiBGXkYnBWls=',
+      username: 'Drax',
+      id: 1,
+    },
+    {
+      uri: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+      username: 'Kyle',
+      id: 2,
+    },
+    {
+      uri: 'https://pbs.twimg.com/profile_images/1383196364792680448/N8CdupEu_400x400.jpg',
+      username: 'Amber',
+      id: 3,
+    },
+    {
+      uri: 'https://www.musicraiser.com/wp-content/uploads/2019/10/28-2.jpg',
+      username: 'Jennie',
+      id: 4,
+    },
+    {
+      uri: 'https://images.unsplash.com/photo-1609132718484-cc90df3417f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFrZSUyMHdvbWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      username: 'Emma',
+      id: 5,
+    },
+    {
+      uri: 'https://wpimg.pixelied.com/blog/wp-content/uploads/2021/06/15175913/Colors-in-Profile-Pictures.jpeg',
+      username: 'Justin',
+      id: 6,
+    },
+  ];
+
   return (
-    <>
+    <NView mb="10">
       <View style={styles.headerContainer}>
-        <TouchableOpacity
+        {/* <TouchableOpacity
           style={styles.leftHeader}
           onPress={() =>
             navigation.navigate('Profile', {
@@ -17,7 +69,6 @@ function PostHeader({navigation}) {
             })
           }>
           <Avatar
-            // bg="lightBlue.400"
             source={{
               uri: contextUser?.profilePic,
             }}
@@ -25,35 +76,93 @@ function PostHeader({navigation}) {
             mr="3">
             <Avatar.Badge bg="green.500" />
           </Avatar>
-          {/* <FastImage
-            style={styles.image}
-            source={{
-              uri: contextUser?.profilePic,
-              // headers: { Authorization: 'someAuthToken' },
-              priority: FastImage.priority.normal,
-            }}
-            resizeMode={FastImage.resizeMode.contain}
-          /> */}
+  
           <View style={styles.userInfo}>
             <Text style={styles.usernameText}>{contextUser?.username}</Text>
             <Text style={styles.email}>{contextUser?.email}</Text>
           </View>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
+        <NView alignItems="center" flexDirection="row">
+          <NText fontFamily="Lato-Bold" color={COLORS.primary} fontSize="2xl">
+            Face
+            <NText fontFamily="Lato-Bold" color={COLORS.white2} fontSize="2xl">
+              Smash
+            </NText>
+          </NText>
+          <NView
+            ml="4"
+            bgColor={COLORS.primary}
+            height="3"
+            width="3"
+            fontFamily="Lato-Bold"
+            rounded="full"></NView>
+        </NView>
 
         <View style={styles.rightHeader}>
           <TouchableOpacity
             hitSlop={{top: 20, bottom: 20, left: 50, right: 50}}
-            onPress={() => navigation.navigate('Add Post')}
             style={styles.searchIcon}>
-            <AddIcon />
+            <SearchIcon />
           </TouchableOpacity>
         </View>
       </View>
-      <View style={styles.feedsContainer}>
-        {/* <Text style={styles.feedsLabel}>Trending</Text> */}
-        <Text style={styles.heading}>Trending</Text>
-      </View>
-    </>
+
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        accessibilityLabel="Stories"
+        borderBottomColor={COLORS.transparentGray}
+        borderBottomWidth="1"
+        pb="3"
+        my="5">
+        <HStack space="3" alignItems="center">
+          <Story noRing uri={contextUser?.profilePic} />
+          {dummyStoryData.map(({uri, username, id}) => (
+            <Story key={id} uri={uri} username={username} />
+          ))}
+        </HStack>
+      </ScrollView>
+
+      <NView
+        justifyContent="space-between"
+        flexDirection="row"
+        alignItems="center">
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('Profile', {
+              providedUserId: contextUser.uid,
+            })
+          }>
+          <Avatar
+            source={{
+              uri: contextUser?.profilePic,
+            }}
+            size="md"
+          />
+        </TouchableOpacity>
+        <Input
+          onPressIn={() => navigation.navigate('Add Post')}
+          borderRadius="full"
+          maxWidth="2/3"
+          ml="-4"
+          // maxHeight="5/6"
+          placeholder={'Write something here...' + '\n' + 'यहाँ कुछ लिखो...'}
+          placeholderTextColor={COLORS.white2}
+          // padding="4"
+          // paddingX="2"
+          // textAlignVertical="top"
+          fontSize="md"
+        />
+        <TouchableOpacity onPress={handleGetImageThenNavigate}>
+          <PhotoIcon width="30" height="30" />
+        </TouchableOpacity>
+      </NView>
+      {/* <View style={styles.feedsContainer}>
+        <NText fontSize="2xl" fontFamily="Lato-Bold">
+          Activities
+        </NText>
+      </View> */}
+    </NView>
   );
 }
 
@@ -62,8 +171,8 @@ export default PostHeader;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#141414',
-    paddingHorizontal: 20,
+    backgroundColor: COLORS.mainBackground,
+    paddingHorizontal: 10,
   },
   scrollView: {},
   innerContainer: {
@@ -111,10 +220,5 @@ const styles = StyleSheet.create({
     color: '#F2F2F2',
     fontSize: 35,
     fontWeight: 'bold',
-  },
-  heading: {
-    fontFamily: 'Lato-Heavy',
-    color: 'white',
-    fontSize: 30,
   },
 });
