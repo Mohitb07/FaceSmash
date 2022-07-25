33489a767325488b4bc9c58b5651ab16e41e1073
mohitb07 <bmohit980@gmail.com>
2022-07-25 22:14:58 +0530

    capitalize words

--- src/firebase/errors.js
+++ src/firebase/errors.js
@@ -3,7 +3,7 @@ export const FIREBASE_ERRORS = {
     'A user with that email already exists',
 
   '[auth/user-not-found] There is no user record corresponding to this identifier. The user may have been deleted.':
-    'Invalid email or password',
+    'Invalid Email or Password',
   '[auth/wrong-password] The password is invalid or the user does not have a password.':
     'Invalid email or password',
   '[auth/invalid-email] The email address is badly formatted.': 'Invalid email',
