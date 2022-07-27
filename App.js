08eee7e8a3d6d4d9ec5b5f8f4a4e037320476977
mohitb07 <bmohit980@gmail.com>
2022-07-27 21:29:03 +0530

    Lazy loaded Component and removed bottomsheet provider

--- App.js
+++ App.js
@@ -6,21 +6,23 @@
  * @flow strict-local
  */
 
-import React, {useEffect} from 'react';
+import React, {useEffect, Suspense} from 'react';
+import {ActivityIndicator} from 'react-native';
 
 import {NativeBaseProvider} from 'native-base';
 import {RecoilRoot} from 'recoil';
-import Toast from 'react-native-toast-message';
-import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen';
+const Toast = React.lazy(() => import('react-native-toast-message'));
+import 'react-native-gesture-handler';
 
 import Navigation, {navigationRef} from './src/Navigation/Root';
 import UserDataContext from './src/Context/userData';
 import BottomSheetProvider from './src/Context/BottomSheet';
 import {theme} from './src/theme';
 import AuthUserContext from './src/Context/auth';
-import BottomSheet from './src/components/BottomSheet';
+import Loader from './src/components/Loader';
+const BottomSheet = React.lazy(() => import('./src/components/BottomSheet'));
 
 const App = () => {
   useEffect(() => {
@@ -28,23 +30,20 @@ const App = () => {
   }, []);
 
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <RecoilRoot>
         <NativeBaseProvider theme={theme}>
           <AuthUserContext>
             <UserDataContext>
-              <BottomSheetProvider>
-                <NavigationContainer ref={navigationRef}>
-                  <Navigation />
-                </NavigationContainer>
-                <BottomSheet />
-              </BottomSheetProvider>
+              <NavigationContainer ref={navigationRef}>
+                <Navigation />
+              </NavigationContainer>
             </UserDataContext>
           </AuthUserContext>
         </NativeBaseProvider>
       </RecoilRoot>
       <Toast />
-    </>
+    </Suspense>
   );
 };
 
