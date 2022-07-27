1a4e9d533a9d233b4c9958b7856e1d43b5696d75
mohitb07 <bmohit980@gmail.com>
2022-07-27 21:31:14 +0530

    Provided the BottomSheet Component Directly

--- src/Context/BottomSheet.js
+++ src/Context/BottomSheet.js
@@ -1,5 +1,6 @@
 import React, {createContext, useState} from 'react';
 import {useDisclose} from 'native-base';
+import BottomSheet from '../components/BottomSheet';
 
 export const BottomSheetContext = createContext();
 
@@ -8,6 +9,7 @@ function BottomSheetProvider({children}) {
   return (
     <BottomSheetContext.Provider value={{isOpen, onOpen, onClose}}>
       {children}
+      <BottomSheet />
     </BottomSheetContext.Provider>
   );
 }
