193828f60b8ca26f4ac4f2d31bbfe3bef021fc45
mohitb07 <bmohit980@gmail.com>
2022-08-03 22:37:47 +0530

    generic datalist for presentation

---
+++ src/components/DataList/index.js
@@ -0,0 +1,68 @@
+import React, {useEffect, useRef, memo} from 'react'
+import {FlatList, RefreshControl} from 'react-native'
+import {COLORS} from '../../constants'
+import Feed from '../Feed'
+
+const DataList = ({
+  dataList = [],
+  Header,
+  Footer,
+  EmptyList,
+  navigation,
+  userLikedPosts,
+  retrieveMore = () => {},
+  onRefresh = () => {},
+  refreshing = false,
+  loading = false,
+}) => {
+  const counter = useRef(0)
+  useEffect(() => {
+    counter.current = counter.current + 1
+  })
+
+  console.log('data list counter', counter.current)
+
+  const renderItems = ({item}) => (
+    <Feed
+      key={item.key}
+      postId={item.key}
+      userProfilePic={item?.userProfile}
+      createdAt={item.createdAt}
+      username={item.username}
+      postTitle={item.title}
+      image={item.image}
+      description={item.description}
+      navigation={navigation}
+      likes={dataList.find(post => post.key === item.key)?.likes}
+      userId={item.user}
+      hasLiked={userLikedPosts.find(post => post.postId === item.key)?.liked}
+      post={item}
+      link={item?.link}
+    />
+  )
+
+  return (
+    <FlatList
+      contentContainerStyle={{paddingBottom: 20}}
+      showsVerticalScrollIndicator={false}
+      data={dataList}
+      ListEmptyComponent={EmptyList}
+      ListHeaderComponent={Header}
+      ListFooterComponent={Footer}
+      onEndReached={retrieveMore}
+      onEndReachedThreshold={0.1}
+      refreshControl={
+        <RefreshControl
+          progressBackgroundColor={COLORS.primary}
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          colors={[COLORS.white2]}
+        />
+      }
+      refreshing={loading}
+      renderItem={renderItems}
+    />
+  )
+}
+
+export default memo(DataList)
