f3fa2fac06473588a5cfe97782aa8ca52b69fc78
mohitb07 <bmohit980@gmail.com>
2022-08-05 22:37:39 +0530

    loader and lastvisible added to global state

--- src/atoms/postAtom.js
+++ src/atoms/postAtom.js
@@ -1,12 +1,14 @@
-import {atom} from 'recoil';
+import {atom} from 'recoil'
 
 export const defaultPostState = {
   selectedPost: null,
   posts: [],
   postLikes: [],
-};
+  loading: true,
+  lastVisible: null,
+}
 
 export const postState = atom({
   key: 'postState',
   default: defaultPostState,
-});
+})
