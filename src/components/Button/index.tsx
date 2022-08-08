08a7270a81d5032f0aac187180dd1c07c819eaef
mohitb07 <bmohit980@gmail.com>
2022-08-08 23:07:03 +0530

    converted to typescript and interfaces added

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,110 @@
+import React, {ReactElement, SVGProps} from 'react'
+
+import {
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native'
+
+import {COLORS} from '../../constants'
+import {PresenceTransition, View} from 'native-base'
+import {TextStyle} from 'react-native'
+
+interface Props {
+  onPress: () => void
+  text: string
+  disabled: boolean
+  color?: string
+  icon?: ReactElement
+  style?: object
+  textStyle?: TextStyle
+  loader: boolean
+  showRing?: boolean
+  loadingText?: string
+}
+
+const Button: React.FC<Props> = ({
+  onPress = () => {},
+  text = '',
+  disabled = false,
+  color = '',
+  icon,
+  style: customStyle = {},
+  textStyle,
+  loader = false,
+  showRing = true,
+  loadingText = 'Loading...',
+}: Props) => {
+  return (
+    <View
+      borderColor={disabled || !showRing ? COLORS.transparent : COLORS.white2}
+      padding="0.5"
+      flexGrow={1}
+      borderWidth="2"
+      rounded="full">
+      <PresenceTransition
+        visible={true}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+          transition: {
+            duration: 250,
+          },
+        }}>
+        <TouchableOpacity
+          disabled={disabled}
+          style={[
+            styles.btn,
+            color.length > 0 && {backgroundColor: color},
+            // disabled && styles.disabled,
+            customStyle,
+          ]}
+          onPress={onPress}>
+          {icon && icon}
+          {loader ? (
+            // <ActivityIndicator color={COLORS.primary} />
+            <Text
+              style={[
+                styles.btnText,
+                // disabled && styles.disabled,
+                textStyle,
+              ]}>
+              {loadingText}
+            </Text>
+          ) : (
+            <Text
+              style={[
+                styles.btnText,
+                // disabled && styles.disabled,
+                textStyle,
+              ]}>
+              {text}
+            </Text>
+          )}
+        </TouchableOpacity>
+      </PresenceTransition>
+    </View>
+  )
+}
+
+export default Button
+
+const styles = StyleSheet.create({
+  btnText: {
+    textAlign: 'center',
+    fontSize: 13,
+    fontFamily: 'Lato-Semibold',
+    color: COLORS.transparentBlack7,
+    marginHorizontal: 10,
+  },
+  btn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15,
+    borderRadius: 50,
+  },
+})
---
+++ src/components/Error/index.tsx
@@ -0,0 +1,44 @@
+import {InfoOutlineIcon} from 'native-base'
+import React from 'react'
+import {Text, View, StyleSheet, TextStyle} from 'react-native'
+
+interface Props {
+  errorStyle?: TextStyle
+  showErrorIcon?: boolean
+  message: string
+}
+
+export default function StyledError({
+  errorStyle,
+  showErrorIcon = false,
+  message,
+}: Props) {
+  return (
+    <View style={styles.errorContainer}>
+      {showErrorIcon && <InfoOutlineIcon style={styles.errorIcon} />}
+      <Text style={[styles.error, errorStyle]}>{message}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  errorContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 3,
+    marginTop: 2,
+  },
+
+  error: {
+    fontSize: 13,
+    color: 'red',
+    fontFamily: 'Lato-Medium',
+    marginLeft: 5,
+  },
+
+  errorIcon: {
+    marginRight: 8,
+    color: 'red',
+  },
+})
