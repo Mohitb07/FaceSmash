8bcca3ad1b505eb5544f2018adbdb0a99ce9b43b
mohitb07 <bmohit980@gmail.com>
2022-08-29 20:51:32 +0530

    generic datalist component

--- src/components/DataList/index.js
+++ src/components/DataList/index.js
@@ -1,28 +1,21 @@
 import React from 'react'
 import {FlatList, RefreshControl} from 'react-native'
 import {COLORS} from '../../constants'
-import EmptyList from '../DataList/EmptyDataList'
 import Feed from '../Feed'
-import Footer from '../FlatList/HomeFeed/DataListFooter'
-import ProfileHeader from '../Header/Profile'
 
 const DataList = ({
   dataList = [],
-  // Header,
-  // Footer,
-  // EmptyList,
+  Header,
+  Footer,
+  EmptyList,
   retrieveMore = () => {},
   onRefresh = () => {},
   refreshing = false,
   loading = false,
   userLikedPosts = [],
-  userId = '',
 }) => {
-  console.log('profile data list render')
-  console.log('user liked posts render', userLikedPosts)
   const renderItems = ({item}) => (
     <Feed
-      key={item.key}
       postId={item.key}
       userProfilePic={item?.userProfile}
       createdAt={item.createdAt}
@@ -32,9 +25,7 @@ const DataList = ({
       description={item.description}
       likes={dataList.find(post => post.key === item.key).likes}
       userId={item.user}
-      // hasLiked={userLikedPosts.find(post => post.postId === item.key)?.liked}
       userLikedPosts={!!userLikedPosts.find(post => post.postId === item.key)}
-      post={item}
       link={item?.link}
     />
   )
@@ -43,11 +34,9 @@ const DataList = ({
       contentContainerStyle={{paddingBottom: 20}}
       showsVerticalScrollIndicator={false}
       data={dataList}
-      ListEmptyComponent={<EmptyList loading={loading} />}
-      ListHeaderComponent={
-        <ProfileHeader totalPosts={dataList?.length} userId={userId} />
-      }
-      ListFooterComponent={<Footer dataList={dataList} loading={loading} />}
+      ListEmptyComponent={EmptyList}
+      ListHeaderComponent={Header}
+      ListFooterComponent={Footer}
       onEndReached={retrieveMore}
       onEndReachedThreshold={0.1}
       refreshControl={
@@ -60,6 +49,7 @@ const DataList = ({
       }
       refreshing={loading}
       renderItem={renderItems}
+      keyExtractor={item => item.key}
     />
   )
 }
