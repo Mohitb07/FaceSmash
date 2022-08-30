d3990baa6ad471b67342b213dab1fae42a838bb1
mohitb07 <bmohit980@gmail.com>
2022-08-30 21:50:06 +0530

    file converted to typescript

---
+++ src/components/DataList/EmptyDataList/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import {Text, View} from 'native-base'
+
+import {COLORS} from '../../../constants'
+const FeedSkeleton = React.lazy(() => import('../../FeedSkeleton'))
+// import FeedSkeleton from '../../FeedSkeleton'
+import {DocumentNotFound} from '../../../SVG'
+
+const EmptyList = ({loading}: {loading: boolean}) => {
+  return loading ? (
+    <>
+      <FeedSkeleton />
+    </>
+  ) : (
+    <View marginY="1/5" justifyContent="center" alignItems="center">
+      <DocumentNotFound height="100px" width="100px" />
+      <Text textAlign="center" color={COLORS.white} fontSize={20} marginTop={5}>
+        Not Enough Posts
+      </Text>
+    </View>
+  )
+}
+
+export default React.memo(EmptyList)
