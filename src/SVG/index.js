6f2baff58d57a6e19a4a931e74683f4545fc4410
mohitb07 <bmohit980@gmail.com>
2022-09-03 22:33:24 +0530

    code refactored

--- src/SVG/index.js
+++ src/SVG/index.js
@@ -690,7 +690,7 @@ export const ThreeDotsIcon = props => (
     {...props}>
     <Path
       d="M10.001 7.8a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 10 7.8zm-7 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 3 7.8zm14 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 17 7.8z"
-      fill={COLORS.primary}
+      fill={props.fillColor || COLORS.primary}
     />
   </Svg>
 )
