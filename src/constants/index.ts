cc51fcfb0d257bfb3f1b0caf5f44ab1b7eaa02ca
mohitb07 <bmohit980@gmail.com>
2022-09-08 22:53:28 +0530

    typescript conversion and default profile pic uri added

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+import theme, {COLORS, SIZES, FONTS} from './theme'
+
+export {theme, COLORS, SIZES, FONTS}
+
+export const DEFAULT_PROFILE_PIC =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG3eLpTAMWO-mtILepXLwg68-IChyGcXJgog&usqp=CAU'
