6894a5c03d7aaa6031f3ba54d615909193a0b6eb
mohitb07 <bmohit980@gmail.com>
2022-09-12 20:53:26 +0530

    converted to typescript

---
+++ src/Context/BottomSheet.tsx
@@ -0,0 +1,37 @@
+import {useDisclose} from 'native-base'
+import React, {createContext, useMemo} from 'react'
+import BottomSheet from '../components/BottomSheet'
+
+interface IBottomSheetState {
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
+}
+
+const initialState = {
+  isOpen: false,
+  onOpen: () => {},
+  onClose: () => {},
+}
+
+export const BottomSheetContext = createContext<IBottomSheetState>(initialState)
+
+function BottomSheetProvider({children}: {children: React.ReactNode}) {
+  const {isOpen, onOpen, onClose} = useDisclose()
+  const values = useMemo(
+    () => ({
+      isOpen,
+      onOpen,
+      onClose,
+    }),
+    [],
+  )
+  return (
+    <BottomSheetContext.Provider value={values}>
+      {children}
+      <BottomSheet />
+    </BottomSheetContext.Provider>
+  )
+}
+
+export default BottomSheetProvider
