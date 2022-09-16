967139603b2df69ffea1647efc52c5ab7a2fc2fc
mohitb07 <bmohit980@gmail.com>
2022-09-16 22:36:46 +0530

    description fontsize increased

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -264,7 +264,7 @@ const styles = StyleSheet.create({
   description: {
     color: '#747474',
     fontFamily: 'Lato-Semibold',
-    fontSize: 11,
+    fontSize: 13,
     marginVertical: 5,
   },
   titleNuser: {
