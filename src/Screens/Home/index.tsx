7faa7c259ad101275c8106ac76c42794fecf42b0
mohitb07 <bmohit980@gmail.com>
2022-09-23 23:10:19 +0530

    component provided directly into the screen

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,13 +1,126 @@
-import React from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import {StyleSheet, View} from 'react-native'
 
-import HomeFeedContainer from '../../components/FlatList/HomeFeed'
+import firestore from '@react-native-firebase/firestore'
+
+import usePagination from '../../hooks/usePagination'
+import {getLastVisibleDocRef} from '../../utils/getLastVisibleDocRef'
+import DataList from '../../components/DataList'
+import Footer from '../../components/DataList/DataListFooter'
+import EmptyList from '../../components/DataList/EmptyDataList'
+import HomeHeader from '../../components/Header/Home'
+import {IDefaultPostState, IPost} from '../../interface'
+import {FEED_LIMIT} from '../../constants'
 import {COLORS} from '../../constants'
 
 const Home = () => {
+  console.log('calling HomeFeed')
+  const [postStateValue, setPostStateValue] = useState<IDefaultPostState>({
+    loading: true,
+    lastVisible: null,
+    posts: [],
+  })
+  const [refreshing, setRefreshing] = useState(false)
+  const {queryMore} = usePagination()
+  const getPosts = useCallback(() => {
+    console.log('calling getPosts home feed')
+    try {
+      firestore()
+        .collection('Posts')
+        .orderBy('createdAt', 'desc')
+        .limit(FEED_LIMIT)
+        .onSnapshot(
+          snapshot => {
+            console.log('calling home feed snapshot')
+            const postList: Array<IPost> = snapshot.docs.map(d => ({
+              key: d.id,
+              createdAt: null,
+              description: '',
+              likes: 0,
+              title: '',
+              user: '',
+              userProfile: '',
+              username: '',
+              imageRef: '',
+              ...d.data(),
+            }))
+            const lastVisiblePostRef = getLastVisibleDocRef(snapshot)
+            setPostStateValue(prev => ({
+              ...prev,
+              posts: postList,
+              lastVisible: lastVisiblePostRef,
+              loading: false,
+            }))
+          },
+          error => {
+            console.log('snapshot home post fetching error', error)
+          },
+        )
+    } catch (error) {
+      console.log('getPosts error', error)
+      setPostStateValue(prev => ({
+        ...prev,
+        loading: false,
+      }))
+    } finally {
+      setRefreshing(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    getPosts()
+  }, [getPosts])
+
+  const onRefresh = () => {
+    try {
+      setRefreshing(true)
+      getPosts()
+    } catch (error) {
+      console.log('On Refresh Error:', error)
+    }
+  }
+
+  const getMoreData = async () => {
+    setPostStateValue(prev => ({
+      ...prev,
+      loading: true,
+    }))
+    try {
+      const {paginatedResult, lastVisibleDocRef} = await queryMore(
+        postStateValue.lastVisible,
+        'Posts',
+      )
+      setPostStateValue(prev => ({
+        ...prev,
+        posts: [...postStateValue.posts, ...paginatedResult],
+        loading: false,
+        lastVisible: lastVisibleDocRef,
+      }))
+    } catch (error) {
+      console.error('ERROR while fetching paginated posts home screen', error)
+    } finally {
+      setPostStateValue(prev => ({
+        ...prev,
+        loading: false,
+      }))
+    }
+  }
   return (
     <View style={styles.container}>
-      <HomeFeedContainer />
+      <DataList
+        dataList={postStateValue.posts}
+        EmptyList={<EmptyList loading={postStateValue.loading} />}
+        Footer={
+          <Footer
+            dataList={postStateValue.posts}
+            loading={postStateValue.loading}
+          />
+        }
+        Header={<HomeHeader />}
+        onRefresh={onRefresh}
+        refreshing={refreshing}
+        retrieveMore={getMoreData}
+      />
     </View>
   )
 }
