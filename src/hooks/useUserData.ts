34d441e0bf204f1b4f77c26ea879f41d36a6edb5
mohitb07 <bmohit980@gmail.com>
2022-09-24 22:48:40 +0530

    wrapping user data context to custom hook

---
+++ src/hooks/useUserData.ts
@@ -0,0 +1,10 @@
+import React, {useContext} from 'react'
+import {UserDataContext} from '../Context/userData'
+
+export default function useUserData() {
+  const context = useContext(UserDataContext)
+  if (!context) {
+    throw new Error('useUserData must be used within a UserDataContextProvider')
+  }
+  return context
+}
