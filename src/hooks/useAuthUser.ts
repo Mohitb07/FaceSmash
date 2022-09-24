e2eb77379e3c1ec230ec3ab628dd48a1a640ca31
mohitb07 <bmohit980@gmail.com>
2022-09-24 22:48:54 +0530

    wrapping auth user context to custom hook

---
+++ src/hooks/useAuthUser.ts
@@ -0,0 +1,10 @@
+import React, {useContext} from 'react'
+import {AuthUserContext} from '../Context/auth'
+
+export default function useAuthUser() {
+  const context = useContext(AuthUserContext)
+  if (!context) {
+    throw new Error('useAuthUser must be used within a AuthUserContextProvider')
+  }
+  return context
+}
