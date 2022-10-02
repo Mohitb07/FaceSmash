600e072f2b29b3e2c64cb417a5ff322b7085ad99
mohitb07 <bmohit980@gmail.com>
2022-10-02 21:45:28 +0530

    module resolver configuration added

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
-};
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'jest-hoist',
+  ],
+}
