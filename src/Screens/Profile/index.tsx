b5e9f14a4bd7dc33a41cabfeb70c58c1b1b49d67
mohitb07 <bmohit980@gmail.com>
2022-10-16 22:29:38 +0530

    code moved to profile header

--- src/Screens/Profile/index.tsx
+++ src/Screens/Profile/index.tsx
@@ -1,43 +1,18 @@
-import React, {useLayoutEffect} from 'react'
-import {StyleSheet, TouchableOpacity, View} from 'react-native'
+import React from 'react'
+import {StyleSheet, View} from 'react-native'
 
-import {RouteProp, useNavigation} from '@react-navigation/native'
-import {useSetRecoilState} from 'recoil'
+import {RouteProp} from '@react-navigation/native'
 
-import {bottomSheetState} from '@/atoms/bottomSheetAtom'
 import ProfileFeed from '@/components/FlatList/ProfileFeed'
 import {COLORS} from '@/constants'
 import BottomSheetProvider from '@/Context/BottomSheet'
-import {ThreeDotsIcon} from '@/SVG'
-import {IBottomSheetState} from '@/interface'
 
 const MyProfile = ({
   route,
 }: {
   route: RouteProp<{params: {providedUserId: string}}, 'params'>
 }) => {
-  const navigation = useNavigation()
   const {providedUserId} = route?.params || null
-  const setBottomSheetStateValue =
-    useSetRecoilState<IBottomSheetState>(bottomSheetState)
-
-  const handleModalState = () =>
-    setBottomSheetStateValue(() => ({
-      type: 'profile',
-      isOpen: true,
-    }))
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity
-          onPress={handleModalState}
-          style={styles.threeDotsIconContainer}>
-          <ThreeDotsIcon fill={COLORS.white} />
-        </TouchableOpacity>
-      ),
-    })
-  }, [navigation])
 
   return (
     <BottomSheetProvider>
@@ -54,8 +29,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.mainBackground,
-  },
-  threeDotsIconContainer: {
-    transform: [{rotate: '90deg'}],
   },
 })
