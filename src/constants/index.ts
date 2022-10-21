8fab01bd894683ae92115c9e36a753a44c85fe92
mohitb07 <bmohit980@gmail.com>
2022-10-21 22:18:47 +0530

    default value colocated

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,20 +8,6 @@ export const DEFAULT_PROFILE_PIC =
 export const FEED_LIMIT = 5
 export const USERS_LIMIT = 5
 
-export const DEFAULT_USER_DETAILS = {
-  bio: '',
-  createdAt: '',
-  email: '',
-  followers: [],
-  followings: [],
-  key: '',
-  lastSignIn: '',
-  profilePic: '',
-  qusername: '',
-  uid: '',
-  username: '',
-}
-
 export const USERS_COLLECTION = 'Users'
 export const POSTS_COLLECTION = 'Posts'
 
