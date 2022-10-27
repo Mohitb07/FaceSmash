74fba0c76610e232afb4d5e5243bc652a253712d
mohitb07 <bmohit980@gmail.com>
2022-10-27 22:09:37 +0530

    auth screen footer links component added

---
+++ src/components/AuthFooter/index.tsx
@@ -0,0 +1,37 @@
+import {RootStackParamList} from '@/Navigation/Root'
+import {useNavigation} from '@react-navigation/native'
+import {NativeStackNavigationProp} from '@react-navigation/native-stack'
+import {Text} from 'native-base'
+import React from 'react'
+
+type AuthScreenNavigationLinkProps = {
+  screen: keyof RootStackParamList
+  screenLabel: string
+  subtitle: string
+}
+
+const AuthScreenNavigationLink: React.FC<AuthScreenNavigationLinkProps> = ({
+  screen,
+  screenLabel,
+  subtitle,
+}) => {
+  const {navigate} =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+  return (
+    <Text
+      fontFamily="Lato-Regular"
+      mt="8"
+      textAlign="center"
+      alignItems="center">
+      {subtitle}{' '}
+      <Text
+        onPress={() => navigate(screen)}
+        mt="10"
+        fontFamily="Lato-Bold"
+        color="primary.400">
+        {screenLabel}
+      </Text>
+    </Text>
+  )
+}
+export default AuthScreenNavigationLink
