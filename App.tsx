877e5d007ca9e9dcf20b3d38aba72b3e7bcf9755
mohitb07 <bmohit980@gmail.com>
2022-10-29 20:51:53 +0530

    feat: firebase push notification setup

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@
  */
 
 import React, {useEffect} from 'react'
+import {Alert} from 'react-native'
 
 import {NavigationContainer} from '@react-navigation/native'
 import {NativeBaseProvider} from 'native-base'
@@ -19,8 +20,48 @@ import AuthUserContext from './src/Context/auth'
 import UserDataContext from './src/Context/userData'
 import Navigation, {navigationRef} from './src/Navigation/Root'
 import {theme} from './src/theme'
+import messaging from '@react-native-firebase/messaging'
 
 const App = () => {
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission()
+
+    console.log('Authorization status:', authStatus)
+
+    return (
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL
+    )
+  }
+
+  useEffect(() => {
+    //@ts-ignore
+    if (requestUserPermission()) {
+      messaging()
+        .getToken()
+        .then(fcmToken => {
+          console.log('FCM TOKEN', fcmToken)
+        })
+    } else {
+      console.log('Not authorized')
+    }
+    messaging()
+      .getInitialNotification()
+      .then(async remoteMessage => {
+        if (remoteMessage) {
+          console.log('get Initial Notification')
+        }
+        console.log('remote res', remoteMessage)
+        Alert.alert('Initial')
+      })
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage))
+    })
+
+    return unsubscribe
+  }, [])
+
   useEffect(() => {
     SplashScreen.hide()
   }, [])
