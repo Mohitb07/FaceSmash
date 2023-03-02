ee4dc323b559e3048734012fe793518b621564b5
Mohitb07 <bmohit980@gmail.com>
2023-03-02 21:49:16 +0530

    refactor: generic pagination function

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,31 +1,17 @@
-import firestore, {
-  FirebaseFirestoreTypes,
-} from '@react-native-firebase/firestore'
+import {FirebaseFirestoreTypes} from '@react-native-firebase/firestore'
 
-import {FEED_LIMIT} from '@/constants'
 import {IPost} from '@/interface'
 import {getLastVisibleDocRef} from '@/utils/getLastVisibleDocRef'
 
 const usePagination = () => {
-  const queryMore = async (
-    lastVisibleDoc: FirebaseFirestoreTypes.DocumentData | null,
-    collection: string,
-  ): Promise<{
-    paginatedResult: IPost[]
-    lastVisibleDocRef: FirebaseFirestoreTypes.DocumentData | null
-  }> => {
-    if (!lastVisibleDoc) {
-      return {paginatedResult: [], lastVisibleDocRef: null}
-    }
-    const queryResult = await firestore()
-      .collection(collection)
-      .orderBy('createdAt', 'desc')
-      .startAfter(lastVisibleDoc)
-      .limit(FEED_LIMIT)
-      .get()
-    const postUserPromises = queryResult.docs.map(d => d.data().user.get())
-    const rawResult = await Promise.all(postUserPromises)
-    const result = rawResult.map(d => d.data())
+  const paginate = async (
+    query: FirebaseFirestoreTypes.Query<FirebaseFirestoreTypes.DocumentData>,
+  ) => {
+    const queryResult = await query.get()
+    const userData = await Promise.all([
+      ...queryResult.docs.map(d => d.data().user.get()),
+    ])
+    const result = userData.map(d => d.data())
     const paginatedResult: Array<IPost> = queryResult.docs.map(
       (doc, index) => ({
         ...(doc.data() as IPost),
@@ -39,43 +25,6 @@ const usePagination = () => {
     return {paginatedResult, lastVisibleDocRef}
   }
 
-  const queryMoreFilter = async (
-    lastVisibleDoc: FirebaseFirestoreTypes.DocumentData | null,
-    collection: string,
-    arg1: string,
-    arg2: FirebaseFirestoreTypes.WhereFilterOp,
-    arg3: string,
-  ): Promise<{
-    paginatedResult: IPost[]
-    lastVisibleDocRef: FirebaseFirestoreTypes.DocumentData | null
-  }> => {
-    if (!lastVisibleDoc) {
-      return {paginatedResult: [], lastVisibleDocRef: null}
-    }
-    const queryResult = await firestore()
-      .collection(collection)
-      .where(arg1, arg2, arg3)
-      .orderBy('createdAt', 'desc')
-      .startAfter(lastVisibleDoc)
-      .limit(FEED_LIMIT)
-      .get()
-    const postUserPromises = queryResult.docs.map(d => d.data().user.get())
-    const rawResult = await Promise.all(postUserPromises)
-    const result = rawResult.map(d => d.data())
-    const paginatedResult: Array<IPost> = queryResult.docs.map(
-      (doc, index) => ({
-        ...(doc.data() as IPost),
-        username: result[index].username,
-        userProfile: result[index].profilePic,
-        key: doc.id,
-      }),
-    )
-    const lastVisibleDocRef = getLastVisibleDocRef(queryResult)
-
-    return {paginatedResult, lastVisibleDocRef}
-  }
-
-  return {queryMore, queryMoreFilter}
+  return {paginate}
 }
-
 export default usePagination
